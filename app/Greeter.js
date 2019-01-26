a705ba56dc2017a51af7da7c7facaa29c5fdb22f
张乐 <zhangle_web@163.com>
2019-01-26 17:25:08 +0800

    ordinary commit

--- app/Greeter.js
+++ app/Greeter.js
@@ -10,21 +10,34 @@
 
 import React, { Component } from 'react'
 import config from './../config.json'
+import { BrowserRouter, Route } from 'react-router-dom'
 import styles from './greeter.css'
 import img from './images/test.jpg'
 import {Img} from './components/index'
+import main from './../node_modules/cellerchan-modal/bundle'
 
 export default class Greeter extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
 
   componentDidMount(){
     console.log(img)
   }
-  
+
+  testMain () {
+    main()
+  }
+
   render() {
     return (
       <div className={styles.root}>
         {config.greetText}
-        <img src={require(`./images/test.jpg`)} />
+        <img src={require(`./images/test.jpg`)}
+          onClick={this.testMain}
+        />
         <img src="./images/test.jpg" />
         <img src={img} />
       </div>
